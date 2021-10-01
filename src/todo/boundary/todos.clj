b3b99d7c2f91cd86af7d5669bea6c5452fd30af9
kero_hirake <k16.h.white@gmail.com>
2021-10-02 00:44:59 +0900

    todoの一覧取得

---
+++ src/todo/boundary/todos.clj
@@ -0,0 +1,11 @@
+(ns todo.boundary.todos
+  (:require [clojure.java.jdbc :as jdbc]))
+
+(defprotocol Todos
+  (get-todos [db]))
+
+(extend-protocol Todos
+  duct.database.sql.Boundary
+
+  (get-todos [{:keys [spec]}]
+    (jdbc/query spec ["SELECT * FROM todos"])))
